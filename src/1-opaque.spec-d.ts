9a39fae8f49c9a7cbc83dfdf093b85e8fd581b2c
Marjorie <aubertmarjorie2@gmail.com>
2025-04-14 17:47:20 +0200

    add tips for exo 1

--- src/1-opaque.spec-d.ts
+++ src/1-opaque.spec-d.ts
@@ -2,7 +2,7 @@ import { describe, expectTypeOf, it } from "vitest";
 import { type Opaque, type UUID } from "./utils";
 
 describe("Opaque", () => {
-  //L'erreur dans le premier it indique uniquement que l'import.
+  //L'erreur dans le premier it indique uniquement que l'import UUID n'est pas utilisé.
   //vous devez fixer le typage dans le fichier utils.ts
   it("Ajouter un type opaque", () => {
     type Basic = number;
@@ -18,6 +18,8 @@ describe("Opaque", () => {
     // type UserId = UUID<"user">;
     // type CompanyId = UUID<"company">;
     // // Go to ./utils.ts to implement working type
+    // // Attention, ici on ne cherche PAS la similitude
+    // // Il est discret mais dans l'expect vous avez un .not.toEqualTypeOf<>
     // expectTypeOf<Basic>().not.toEqualTypeOf<UserId>();
     // expectTypeOf<UserId>().not.toEqualTypeOf<CompanyId>();
   });
@@ -26,6 +28,8 @@ describe("Opaque", () => {
   //   type OpaqueUserId = Opaque<string, "user">;
   //   type UserUUID = UUID<"user">;
 
+  // // Attention, ici comme dans le cas précédent on ne cherche PAS la similitude
+  // // Il est discret mais dans l'expect vous avez un .not.toEqualTypeOf<>
   //   expectTypeOf<OpaqueUserId>().not.toEqualTypeOf<UserUUID>();
   // });
 });
