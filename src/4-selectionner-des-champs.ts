544f1d738ac56e8e5e2ef0107d83d0cac69bfaa2
Johan ROUVE <johan.rouve@gmail.com>
2025-04-15 10:12:37 +0200

    Reformat code

--- src/4-selectionner-des-champs.ts
+++ src/4-selectionner-des-champs.ts
@@ -4,28 +4,28 @@ import {buildContext, type CustomerDatabase, selectFields, selectFrom, type Shop
  * Selectionner des champs dans une table
  */
 export function testSelectFields() {
-  /**
-   * On peut sélectionner parmi tous les champs d'une table
-   */
-  //   const customerContext = buildContext<CustomerDatabase>();
-  // const selectUsersQuery = selectFrom(customerContext, "users");
-  // selectFields(selectUsersQuery, [
-  //   "id",
-  //   "firstName",
-  //   "lastName",
-  //   "birthDate",
-  // ]);
-  // // @ts-expect-error
-  // selectFields(selectUsersQuery, ["idi"]);
-  //
-  // const selectCompaniesQuery = selectFrom(customerContext, "companies");
-  // selectFields(selectCompaniesQuery, ["id", "name"]);
-  // // @ts-expect-error
-  // selectFields(selectCompaniesQuery, ["id", "firstName"]);
-  //
-  // const shoppingContext = buildContext<ShoppingDatabase>();
-  // const selectProductsQuery = selectFrom(shoppingContext, "products");
-  // selectFields(selectProductsQuery, ["id", "name", "description"]);
-  // // @ts-expect-error
-  // selectFields(selectProductsQuery, ["id", "firstName"]);
+    /**
+     * On peut sélectionner parmi tous les champs d'une table
+     */
+    // const customerContext = buildContext<CustomerDatabase>();
+    // const selectUsersQuery = selectFrom(customerContext, "users");
+    // selectFields(selectUsersQuery, [
+    //     "id",
+    //     "firstName",
+    //     "lastName",
+    //     "birthDate",
+    // ]);
+    // // @ts-expect-error
+    // selectFields(selectUsersQuery, ["idi"]);
+    //
+    // const selectCompaniesQuery = selectFrom(customerContext, "companies");
+    // selectFields(selectCompaniesQuery, ["id", "name"]);
+    // // @ts-expect-error
+    // selectFields(selectCompaniesQuery, ["id", "firstName"]);
+    //
+    // const shoppingContext = buildContext<ShoppingDatabase>();
+    // const selectProductsQuery = selectFrom(shoppingContext, "products");
+    // selectFields(selectProductsQuery, ["id", "name", "description"]);
+    // // @ts-expect-error
+    // selectFields(selectProductsQuery, ["id", "firstName"]);
 }
