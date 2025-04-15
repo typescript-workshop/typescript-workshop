8de1615283f7672c75aa8d2f510454ed5b1785fd
Bastien Tran <bastien.tran@comet.team>
2025-04-15 19:44:48 +0200

    provide filterable context to 'where'

--- src/6-filtrer-des-lignes.ts
+++ src/6-filtrer-des-lignes.ts
@@ -1,4 +1,4 @@
-import {buildContext, type CustomerDatabase, selectFrom, where} from "./db";
+import {buildContext, type CustomerDatabase, selectAll, selectFrom, where} from "./db";
 
 /**
  * Filtrer les lignes d'une table
@@ -7,15 +7,16 @@ export function testWhere() {
     /**
      * On peut fournir un critère afin de ne récupérer que des lignes qui le satisfont
      */
-    // const customerContext = buildContext<CustomerDatabase>();
-    // const selectUsersQuery = selectFrom(customerContext, "users");
-    //
-    // where(selectUsersQuery, "firstName", "=", "Max");
-    // where(selectUsersQuery, "birthDate", "=", new Date());
-    // // @ts-expect-error
-    // where(selectUsersQuery, "firstName", "=", 42);
-    // // @ts-expect-error
-    // where(selectUsersQuery, "birthDate", "=", "not a date");
-    // // @ts-expect-error
-    // where(selectUsersQuery, "first_name", "=", "Max");
+    const customerContext = buildContext<CustomerDatabase>();
+    const selectUsersQuery = selectFrom(customerContext, "users");
+    const selectAllUsersFieldsQuery = selectAll(selectUsersQuery);
+    
+    where(selectAllUsersFieldsQuery, "firstName", "=", "Max");
+    where(selectAllUsersFieldsQuery, "birthDate", "=", new Date());
+    // @ts-expect-error
+    where(selectAllUsersFieldsQuery, "firstName", "=", 42);
+    // @ts-expect-error
+    where(selectAllUsersFieldsQuery, "birthDate", "=", "not a date");
+    // @ts-expect-error
+    where(selectAllUsersFieldsQuery, "first_name", "=", "Max");
 }
