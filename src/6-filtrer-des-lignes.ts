22f758d2702ce014354d8b70668e4e96d8812cab
Bastien Tran <bastien.tran@comet.team>
2025-04-15 21:17:19 +0200

    comment out n°6

--- src/6-filtrer-des-lignes.ts
+++ src/6-filtrer-des-lignes.ts
@@ -7,16 +7,16 @@ export function testWhere() {
     /**
      * On peut fournir un critère afin de ne récupérer que des lignes qui le satisfont
      */
-    const customerContext = buildContext<CustomerDatabase>();
-    const selectUsersQuery = selectFrom(customerContext, "users");
-    const selectAllUsersFieldsQuery = selectAll(selectUsersQuery);
+    // const customerContext = buildContext<CustomerDatabase>();
+    // const selectUsersQuery = selectFrom(customerContext, "users");
+    // const selectAllUsersFieldsQuery = selectAll(selectUsersQuery);
     
-    where(selectAllUsersFieldsQuery, "firstName", "=", "Max");
-    where(selectAllUsersFieldsQuery, "birthDate", "=", new Date());
-    // @ts-expect-error
-    where(selectAllUsersFieldsQuery, "firstName", "=", 42);
-    // @ts-expect-error
-    where(selectAllUsersFieldsQuery, "birthDate", "=", "not a date");
-    // @ts-expect-error
-    where(selectAllUsersFieldsQuery, "first_name", "=", "Max");
+    // where(selectAllUsersFieldsQuery, "firstName", "=", "Max");
+    // where(selectAllUsersFieldsQuery, "birthDate", "=", new Date());
+    // // @ts-expect-error
+    // where(selectAllUsersFieldsQuery, "firstName", "=", 42);
+    // // @ts-expect-error
+    // where(selectAllUsersFieldsQuery, "birthDate", "=", "not a date");
+    // // @ts-expect-error
+    // where(selectAllUsersFieldsQuery, "first_name", "=", "Max");
 }
