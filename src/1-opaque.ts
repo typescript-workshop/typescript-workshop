2d87e2b5cf7276793d104c992753e80b81d638c9
Marjorie <aubertmarjorie2@gmail.com>
2025-09-08 11:51:01 +0200

    update opaque uuid test

--- src/1-opaque.ts
+++ src/1-opaque.ts
@@ -5,38 +5,40 @@ import { type Opaque, type UUID } from "./utils";
  * Opaque
  */
 export function testOpaque() {
-    /**
-     * Ajouter un type opaque
-     *
-     * L'erreur dans le premier it indique uniquement que l'import UUID n'est pas utilisé.
-     * vous devez fixer le typage dans le fichier utils.ts
-     */
-    // type NumberAlias = number;
-    // type UserId = Opaque<number, "user">;
-    // type CompanyId = Opaque<number, "company">;
-    //
-    // expectTypeOf<NumberAlias>().not.toEqualTypeOf<UserId>();
-    // expectTypeOf<UserId>().not.toEqualTypeOf<CompanyId>();
+  /**
+   * Ajouter un type opaque
+   *
+   * L'erreur dans le premier it indique uniquement que l'import UUID n'est pas utilisé.
+   * vous devez fixer le typage dans le fichier utils.ts
+   */
+  type NumberAlias = number;
+  type UserId = Opaque<number, "user">;
+  type CompanyId = Opaque<number, "company">;
 
-    /**
-     * Pouvoir utiliser un uuid
-     *
-     * Go to ./utils.ts to implement working type
-     * Attention, ici on ne cherche PAS la similitude
-     * Il est discret mais dans l'expect vous avez un .not.toEqualTypeOf<>
-     */
-    // type StringAlias = string;
-    // type UserUUID = UUID<"user">;
-    // type CompanyUUID = UUID<"company">;
-    //
-    // expectTypeOf<StringAlias>().not.toEqualTypeOf<UserUUID>();
-    // expectTypeOf<UserUUID>().not.toEqualTypeOf<CompanyUUID>();
+  expectTypeOf<NumberAlias>().not.toEqualTypeOf<UserId>();
+  expectTypeOf<UserId>().not.toEqualTypeOf<CompanyId>();
 
-    /**
-     * Pouvoir ajouter séparément uuid et types opaque
-     *
-     * Attention, ici comme dans le cas précédent on ne cherche PAS la similitude
-     * Il est discret mais dans l'expect vous avez un .not.toEqualTypeOf<>
-     */
-    // expectTypeOf<UserId>().not.toEqualTypeOf<UserUUID>();
+  /**
+   * Pouvoir utiliser un uuid
+   *
+   * Go to ./utils.ts to implement working type
+   * Attention, ici on ne cherche PAS la similitude
+   * Il est discret mais dans l'expect vous avez un .not.toEqualTypeOf<>
+   */
+  type StringAlias = string;
+  type UserUUID = UUID<"user">;
+  type CompanyUUID = UUID<"company">;
+  type CompanyName = Opaque<string, "company">;
+
+  expectTypeOf<StringAlias>().not.toEqualTypeOf<UserUUID>();
+  expectTypeOf<UserUUID>().not.toEqualTypeOf<CompanyUUID>();
+  expectTypeOf<CompanyName>().not.toEqualTypeOf<CompanyUUID>();
+
+  /**
+   * Pouvoir ajouter séparément uuid et types opaque
+   *
+   * Attention, ici comme dans le cas précédent on ne cherche PAS la similitude
+   * Il est discret mais dans l'expect vous avez un .not.toEqualTypeOf<>
+   */
+  expectTypeOf<UserId>().not.toEqualTypeOf<UserUUID>();
 }
